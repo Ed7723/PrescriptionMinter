1350e0b8fdaca973129d4c6976a9ac1c146f2067
Ed7723 <linde2222@hotmail.com>
2022-11-15 22:31:52 -0500

    Update: Added custom colors to tailwindCSS

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors:{
+    'blueOne':'#118df0',
+    'blueTwo':'#41a4f3',
+    'blueThree':'#0c71c2',
+    'blueFour':'#71bbf6',
+    'whiteOne':'#ffffff',
+    'whiteTwo':'#e6e6e6'
+    },
     extend: {},
   },
   plugins: [],
