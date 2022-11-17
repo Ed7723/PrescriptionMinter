9c6d623f045b4e19d87449f62cea49765f54b7ab
Mongkool <mongkolsong1999@gmail.com>
2022-11-16 21:10:27 -0800

    Create: script to connect to MongoDB cluster and create patients in the database

---
+++ BackEnd/database.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require('mongodb');
+
+async function main() {
+
+    const uri ="mongodb+srv://Mongko:nL7yt2vWrxRB1hJV@patient-prescription-da.mlaoml8.mongodb.net/?retryWrites=true&w=majority"; // link to MongoDB cluster
+
+    const client = new MongoClient(uri); // creates connection to MongoDB cluster
+
+    try {
+        await client.connect();
+        
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+}
+
+async function createPatient(client, newPatient){
+
+    const result = await client.db("prescriptoken_db").collection("patients").insertOne(newPatient);
+
+    console.log(`New listing created with the following id: ${result.insertedId}`); // returns patient's account seed
+}
+
+main().catch(console.error);
