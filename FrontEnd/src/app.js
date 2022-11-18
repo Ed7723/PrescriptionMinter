b92d3b1e4cd0ae1e133cbb6fe527996cc1a7af90
Ed7723 <linde2222@hotmail.com>
2022-11-18 14:28:12 -0500

    Update: Updated app.js and prescription.js

--- FrontEnd/src/app.js
+++ FrontEnd/src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import "./index.css"
 import Navbar from './Nav/navbar'
+import Inputform from './inputform'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Inputform />
       </div>
     )
   }
