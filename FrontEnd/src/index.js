adb87d71d1540dae5c8b8332f7d60193f2624be3
Ed7723 <linde2222@hotmail.com>
2022-11-19 09:29:26 -0500

    Update:Inputform template created

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './app'
+import Inputform from './inputform'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Inputform />, document.getElementById('inputform'))
