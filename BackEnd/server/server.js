f0f795152f380fd0d74e4e5e9c9088bc171177c8
Ed7723 <linde2222@hotmail.com>
2022-11-20 13:50:39 -0500

    Update: Some updates for express and middleware

--- BackEnd/server/server.js
+++ BackEnd/server/server.js
@@ -5,9 +5,9 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require("./routes/routing"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("./db/database");
  
 app.listen(port, () => {
   // perform a database connection when server starts
