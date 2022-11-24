317c8de86c77a16c8171f989b480ef55ddbaf493
Ed7723 <linde2222@hotmail.com>
2022-11-24 01:22:25 -0500

    Create: Creating navlinks for navigation

--- FrontEnd/src/app.js
+++ FrontEnd/src/app.js
@@ -1,15 +1,28 @@
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 import { Features } from "./components/features";
-import Inputform from "./components/inputform";
+import Inputform  from "./components/inputform";
+import Database from "./components/database";
+import {BrowserRouter} from 'react-router-dom';
+import {Route, Switch} from 'react-router';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
+      <Switch>
+        <Route path = "/database">
+          <Database />
+        </Route>
+        <Route path = "/">
+          <Database />
+        </Route>
+      </Switch>
       <Hero />
       <Features />
       <Inputform/>
+    </BrowserRouter>
     </>
   );
 }
