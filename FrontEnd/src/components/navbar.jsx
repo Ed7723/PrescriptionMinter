b8a5bc29bcb45ccd795c97c1d033ca5ea700a8fe
Ed7723 <linde2222@hotmail.com>
2022-11-25 02:36:48 -0500

    Update: Changed sidebar width from 70 to 20

--- FrontEnd/src/components/navbar.jsx
+++ FrontEnd/src/components/navbar.jsx
@@ -38,7 +38,7 @@ export const Navbar = () => {
 
                 </div>
 
-                <div className={!nav ? 'fixed left-0 top-0 w-[70%] h-full border-r border-r-gray-900 bg-white' : 'fixed left-[-100%] top-0 w-[70%] h-full border-r border-r-gray-900 bg-white'}>
+                <div className={!nav ? 'fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-white' : 'fixed left-[-100%] top-0 w-[70%] h-full border-r border-r-gray-900 bg-white'}>
                     <h1 className="w-full text-3xl font-bold mx-6 mt-8 mb-2">Prescriptoken</h1>
                     <ul className='p-4 font-semibold'>
                         <li className="p-4 border-b border-gray-600"><NavLink to="/database">Patient Database</NavLink></li>
