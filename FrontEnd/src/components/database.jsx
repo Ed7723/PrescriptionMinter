647fe7316803d726a323e2716604037c74009ce7
Ed7723 <linde2222@hotmail.com>
2022-11-27 02:17:06 -0500

    Update: Imported table to database

--- FrontEnd/src/components/database.jsx
+++ FrontEnd/src/components/database.jsx
@@ -1,8 +1,8 @@
 //This javascript file will import a table and then load it with relevant information.
-export default function DatabaseTable() {
+import Table from './table';
+
+export function DatabaseTable() {
   return (
-    <>
-        <h1>This is where the database will be</h1>
-    </>
+        <Table/>
   );
 }
