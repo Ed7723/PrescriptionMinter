6c2fd6a2d044fd911a00e45ef94ba5808eb234df
Ed7723 <linde2222@hotmail.com>
2022-11-27 02:16:25 -0500

    Update: Added extra routes and changed import

--- FrontEnd/src/app.js
+++ FrontEnd/src/app.js
@@ -1,10 +1,11 @@
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 import { Features } from "./components/features";
-import Inputform  from "./components/inputform";
-import DatabaseTable from "./components/database";
+import {Inputform}  from "./components/inputform";
+import {DatabaseTable} from "./components/database";
 import {BrowserRouter, Route,Routes} from 'react-router-dom';
-import Deleteform from "./components/deleteform";
+import {Deleteform}from "./components/deleteform";
+import {About} from "./components/about"
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path = "/database" element={<DatabaseTable />} ></Route>
         <Route path = "/input" element={<Inputform/>} ></Route>
+        <Route path = "/about" element={<About/>} ></Route>
+        <Route path = "/features" element={<Features/>} ></Route>
       </Routes>
       <Hero />
       <Features />
