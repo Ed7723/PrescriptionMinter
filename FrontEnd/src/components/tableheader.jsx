f9d4433bc64a9bdd5b7209aa6845b0634b816a6c
Ed7723 <linde2222@hotmail.com>
2022-12-02 00:07:00 -0500

    Create: created table header file

---
+++ FrontEnd/src/components/tableheader.jsx
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+        Header:'First Name',
+        accessor:'firstName',
+    },
+    {
+        Header:'Last Name',
+        accessor:'lastName',
+    },
+    {
+        Header:'Date Of Birth',
+        accessor:'dob',
+    },
+    {
+        Header:'Sex',
+        accessor:'sex',
+    },
+    {
+        Header:'ID',
+        accessor:'patientID',
+    },
+];
