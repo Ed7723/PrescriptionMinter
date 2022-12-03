6b202232dce0b89bb3f3c4dc2a05688f8a14cb4b
Ed7723 <linde2222@hotmail.com>
2022-12-02 22:01:32 -0500

    Update: Table update, still needs styling

--- FrontEnd/src/components/table.jsx
+++ FrontEnd/src/components/table.jsx
@@ -1,14 +1,20 @@
 //This component will load a table
 import {COLUMNS} from './tableheader';
-import {useTable} from 'react-table';
+import {useTable, useGlobalFilter} from 'react-table';
 import { useState, useEffect, useMemo } from 'react';
 export function Table() {
     const[data, setData] = useState([]);
-
     const columns = useMemo(()=> COLUMNS,[]);
-    const tableInstance =useTable({columns,data});
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-        useTable({ columns, data });
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        state,
+        setGlobalFilter,
+        prepareRow,
+    } = useTable({ columns, data }, useGlobalFilter);
+    const globalFilter = state;
 
     return (
         <>
@@ -21,11 +27,8 @@ export function Table() {
                         <div className="text-end">
                             <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                                 <div className=" relative ">
-                                    <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
+                                    <input type="text" id="form-subscribe-Filter" value={globalFilter || ''} onChange={(e) => setGlobalFilter(e.target.value)} className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
                                 </div>
-                                <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                                    Filter
-                                </button>
                             </form>
                         </div>
                     </div>
