a44dfe5b441e7f14bc7f41e2fecefa71ee2477aa
Ken <kjliu093@gmail.com>
2022-12-04 15:07:23 -0800

    changed login page

--- FrontEnd/src/components/login.jsx
+++ FrontEnd/src/components/login.jsx
@@ -5,7 +5,7 @@ export default function login() {
         <div className="h-screen flex justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
-                    <h2 className="text-center text-3xl font-bold text-gray-900">Sign In</h2>
+                    <h2 className="text-center text-3xl font-bold text-gray-900">Sign In to Prescribe</h2>
                 </div>
                 <form className="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" defaultValue="true" />
