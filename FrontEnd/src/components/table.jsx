93c58d48d0ea773fe895edad6e8382a9f7c31204
Ed7723 <linde2222@hotmail.com>
2022-12-06 17:53:14 -0500

    Update: Removed filter function temporarily

--- FrontEnd/src/components/table.jsx
+++ FrontEnd/src/components/table.jsx
@@ -1,9 +1,14 @@
 //This component will load a table
 import {COLUMNS} from './tableheader';
-import {useTable, useFilters, useGlobalFilter} from 'react-table';
+import {useTable} from 'react-table';
 import { useState, useEffect, useMemo } from 'react';
 export function Table() {
-    const[data, setData] = useState([]);
+    const[data, setData] = useState({
+        firstName:"",
+        lastName:"",
+        sex:"",
+        dateOfBirth:"",
+    });
 
     const columns = useMemo(()=> COLUMNS,[]);
     const {
@@ -11,25 +16,23 @@ export function Table() {
         getTableBodyProps,
         headerGroups,
         rows,
-        state,
-        setGlobalFilter,
         prepareRow,
-    } = useTable({ columns, data }, useFilters, useGlobalFilter);
-    const globalFilter = state;
+    } = useTable({ columns, data });
 
     useEffect(()=>{
         const fetchData = async ()=>{
-            const response =  await fetch("http://localhost:8000/routing/")
+            const response =  await fetch("http://localhost:8000/routing/");
                 if (!response.ok) {
                     const message = `An error occurred: ${response.statusText}`;
                     window.alert(message);
                     return;
                 };
-                const records = await response.json();
+                const records = await response;
+                console.log(records);
                 setData(records);
         };
         fetchData();
-    },[]);
+    });
 
     return (
         <>
@@ -42,7 +45,7 @@ export function Table() {
                         <div className="text-end">
                             <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                                 <div className=" relative ">
-                                    <input type="text" id="form-subscribe-Filter" value={globalFilter || ''} onChange={(e) => setGlobalFilter(e.target.value)} className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
+                                    <input type="text" id="form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
                                 </div>
                             </form>
                         </div>
@@ -81,34 +84,6 @@ export function Table() {
                                     })}
                                 </tbody>
                             </table>
-                            <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
-                                <div className="flex items-center">
-                                    <button type="button" className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
-                                        <svg width="9" fill="currentColor" height="8" className="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                    <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
-                                        1
-                                    </button>
-                                    <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                                        2
-                                    </button>
-                                    <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
-                                        3
-                                    </button>
-                                    <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                                        4
-                                    </button>
-                                    <button type="button" className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
-                                        <svg width="9" fill="currentColor" height="8" className="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
--- FrontEnd/src/components/tableheader.jsx
+++ FrontEnd/src/components/tableheader.jsx
@@ -1,32 +1,20 @@
 //This component is used as a dynamic header file to change the headers of the table in tableform as necessary.
 //Header is the display variable, accessor is the id we use to target that part and the filter is the filter used.
-
-import TableFilter from "./tablefilter";
-
 export const COLUMNS = [
     {
         Header:'First Name',
         accessor:'firstName',
-        Filter: TableFilter,
     },
     {
         Header:'Last Name',
         accessor:'lastName',
-        Filter: TableFilter,
     },
     {
         Header:'Date Of Birth',
-        accessor:'dob',
-        Filter: TableFilter,
+        accessor:'dateOfBirth',
     },
     {
         Header:'Sex',
         accessor:'sex',
-        Filter: TableFilter,
-    },
-    {
-        Header:'ID',
-        accessor:'patientID',
-        Filter: TableFilter,
     },
 ];
