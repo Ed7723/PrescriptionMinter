dec29de5ab712cc4027700d302812e9382cfa026
Ed7723 <linde2222@hotmail.com>
2022-12-10 18:17:31 -0500

    Update: Changed placeholder text

--- FrontEnd/src/components/prescriptionform.jsx
+++ FrontEnd/src/components/prescriptionform.jsx
@@ -44,17 +44,17 @@ export function Prescriptform() {
             <form onSubmit={onSubmit}>
                 <div className="grid gap-6 mb-6 lg:grid-cols-2">
                     <div>
-                        <label htmlFor="drugName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
+                        <label htmlFor="drugName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Drug Name</label>
                         <input type="text" id="drugName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" value={form.drugName} onChange={(e) => updateForm({ drugName:e.target.value })} required />
                     </div>
                     <div>
-                        <label htmlFor="patientName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last name</label>
+                        <label htmlFor="patientName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Patient Name</label>
                         <input type="text" id="patientName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" value={form.patientName} onChange={(e) => updateForm({ patientName: e.target.value })} required />
                     </div>
                 </div>
                 <div className="grid gap-6 mb-6">
                     <div>
-                        <label htmlFor="expiryDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Date of Birth</label>
+                        <label htmlFor="expiryDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Expiry Date</label>
                         <input type="date" id="expiryDate" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" value={form.expiryDate} onChange={(e) => updateForm({ expiryDate: e.target.value })} required />
                     </div>
                     <button type="submit" className="text-whiteTwo bg-lightBlue hover:bg-brightBlue focus:ring-4 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
